545ddb1e392c34fcb76dd62b2e37991da64a0f45
pawankingofbeast <pawankingofbeast@gmail.com>
2016-09-24 01:03:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('.article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article one');
 });
 
 
-app.get('.article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article two');
 });
 
 
 
-app.get('.article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three');
 });
 
